4aac7046993d78a5982a21327545baaa2deb5090
RJ Cunanan <rjlugtuc@gmail.com>
2024-12-22 17:23:49 -0800

    Converted SCSS files of regular components into CSS-In-JS styled components in JSX files.

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,16 +1,14 @@
-import { Link } from 'react-router-dom';
-
 import ProductCard from '../product-card/product-card.component';
 
-import './category-preview.styles.scss';
+import { CategoryPreviewContainer, Title, Preview } from './category-preview.styles';
 
 const CategoryPreview = ({ title, products }) => {
     return (
-        <div className='category-preview-container'>
+        <CategoryPreviewContainer>
             <h2>
-                <Link className='title' to={title}>{title.toUpperCase()}</Link>
+                <Title to={title}>{title.toUpperCase()}</Title>
             </h2>
-            <div className='preview'>
+            <Preview>
                 {
                     // Display only the first four items of each category of products.
                     // Note: the underscore "_" refers to the product that we don't use
@@ -21,8 +19,8 @@ const CategoryPreview = ({ title, products }) => {
                             <ProductCard key={product.id} product={product} />
                         ))
                 }
-            </div>
-        </div>
+            </Preview>
+        </CategoryPreviewContainer>
     )
 }
 
